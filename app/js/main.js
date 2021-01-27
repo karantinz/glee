6a3c3c5763b61c967c43ed248b9acee6935834de
Danila <karantinz@mail.ru>
2021-01-28 01:20:54 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -6,18 +6,35 @@ $(function(){
 
     $('.partners__inner').slick({
         arrows: false,
-        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    });
+
+    $('.menu__btn').on('click', function () {
+        $('.menu__list, .menu__btn').toggleClass('menu__btn--active');
+    });
+
+    $('.menu__link').on('click', function () {
+        $('.menu__list, .menu__btn').toggleClass('menu__btn--active');
+    });
+
+    $('.menu__btn').on('click', function () {
+        $('.user-nav').toggleClass('user-nav__btn--active');
+    });
+
+    $('.menu__link').on('click', function () {
+        $('.user-nav').toggleClass('user-nav__btn--active');
     });
 
 
     var mixer1 = mixitup('.product__content', {
         selectors: {
-            control: '.product__item'
+            control: '.goods__btn--product'
         }
     });
     var mixer2 = mixitup('.design__inner', {
         selectors: {
-            control: '.design__item'
+            control: '.goods__btn--design'
         }
     });
 })
